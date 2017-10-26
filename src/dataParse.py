ea51d5b51d003a226aeca71736008f0e39aa602d
William Wu <williamwu0013@gmail.com>
2017-10-26 01:17:04 -0400

    Added data parse function.

--- src/dataParse.py
+++ src/dataParse.py
@@ -1,5 +1,31 @@
 import sys
+import bisect
 
-dataStream = open("../insight_testsuite/tests/test_1/input/itcont.txt", mode='r')
-print(dataStream)
+dataStream = open("insight_testsuite/tests/test_1/input/itcont.txt", mode='r')
+medianByZip = open("output/medianvals_by_zip.txt", mode='w')
+medianBydate = open("output/medianvals_by_date.txt", mode='w')
 
+def parse_data(data):
+    data_array = data.split('|')
+    data_dict = {"cmte_id": data_array[0],
+                 "zip_code": data_array[10][:5],
+                 "transaction_dt" : data_array[13],
+                 "transaction_amt" : data_array[14],
+                 "other_id" : data_array[15]}
+    return data_dict
+
+'''
+def return_median(sorted_list):
+    length = len(sorted_list)
+    middle = length / 2
+    if middle % 2 == 0:
+        median = middle - 1
+    else:
+        median = middle
+    print(sorted_list[median])
+'''
+
+sorted_list = []
+for data in dataStream:
+    donator_data = parse_data(data)
+    print(donator_data)
