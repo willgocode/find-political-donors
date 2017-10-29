d6ca3f36a8bbb7980d7d238ab495b4e21c652012
William Wu <williamwu0013@gmail.com>
2017-10-29 17:57:44 -0400

    Updated run.sh to point to new python script.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
-python ./src/data_parse.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
+python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 
