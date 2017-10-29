21136f852cbfc49b89e952b30e4c6b7fc5618a79
William Wu <williamwu0013@gmail.com>
2017-10-29 17:48:03 -0400

    Updated generate script to accomodate new tests.

--- generate_all_files.sh
+++ generate_all_files.sh
@@ -21,4 +21,12 @@ python ./src/find_political_donors.py ./insight_testsuite/tests/test_3/input/itc
 python ./src/find_political_donors.py ./insight_testsuite/tests/test_4/input/itcont.txt\
                            ./insight_testsuite/tests/test_4/output/medianvals_by_zip.txt\
                            ./insight_testsuite/tests/test_4/output/medianvals_by_date.txt
+#test 5
+python ./src/find_political_donors.py ./insight_testsuite/tests/test_5/input/itcont.txt\
+                           ./insight_testsuite/tests/test_5/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_5/output/medianvals_by_date.txt
+#test 6
+python ./src/find_political_donors.py ./insight_testsuite/tests/test_6/input/itcont.txt\
+                           ./insight_testsuite/tests/test_6/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_6/output/medianvals_by_date.txt
 
