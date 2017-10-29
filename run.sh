74a90b6378fdd39069e58b68477f461482103845
William Wu <williamwu0013@gmail.com>
2017-10-29 16:07:23 -0400

    Removed unnecessary lines from run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,3 @@
 #
 python ./src/data_parse.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 
-#uncomment the following to run tests and generate other files
-'
-python ./src/data_parse.py ./insight_testsuite/tests/test_1/input/itcont.txt\
-                            ./insight_testsuite/tests/test_1/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_1/output/medianvals_by_date.txt
-python ./src/data_parse.py ./insight_testsuite/tests/test_2/input/itcont.txt\
-                            ./insight_testsuite/tests/test_2/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_2/output/medianvals_by_date.txt
-python ./src/data_parse.py ./insight_testsuite/tests/test_3/input/itcont.txt\
-                            ./insight_testsuite/tests/test_3/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_3/output/medianvals_by_date.txt
-'
