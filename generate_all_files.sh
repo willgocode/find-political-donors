2fdcf0e72f078d4cbe2631980ff3832cd782ebc2
William Wu <williamwu0013@gmail.com>
2017-10-29 16:30:50 -0400

    Updated for test 4.

--- generate_all_files.sh
+++ generate_all_files.sh
@@ -7,14 +7,18 @@ python ./src/data_parse.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./o
 
 #test 1
 python ./src/data_parse.py ./insight_testsuite/tests/test_1/input/itcont.txt\
-                            ./insight_testsuite/tests/test_1/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_1/output/medianvals_by_date.txt
+                           ./insight_testsuite/tests/test_1/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_1/output/medianvals_by_date.txt
 #test 2
 python ./src/data_parse.py ./insight_testsuite/tests/test_2/input/itcont.txt\
-                            ./insight_testsuite/tests/test_2/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_2/output/medianvals_by_date.txt
+                           ./insight_testsuite/tests/test_2/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_2/output/medianvals_by_date.txt
 #test 3
 python ./src/data_parse.py ./insight_testsuite/tests/test_3/input/itcont.txt\
-                            ./insight_testsuite/tests/test_3/output/medianvals_by_zip.txt\
-                            ./insight_testsuite/tests/test_3/output/medianvals_by_date.txt
+                           ./insight_testsuite/tests/test_3/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_3/output/medianvals_by_date.txt
+#test 4
+python ./src/data_parse.py ./insight_testsuite/tests/test_4/input/itcont.txt\
+                           ./insight_testsuite/tests/test_4/output/medianvals_by_zip.txt\
+                           ./insight_testsuite/tests/test_4/output/medianvals_by_date.txt
 
