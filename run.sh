9df7a3e0531dac95eead1101b8dcf29e9fd8d1ab
William Wu <williamwu0013@gmail.com>
2017-10-28 22:44:34 -0400

    Updated script with commented area for generating additional test files.

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
 python ./src/data_parse.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt
 
+#uncomment the following to run tests and generate other files
+'
+python ./src/data_parse.py ./insight_testsuite/tests/test_1/input/itcont.txt\
+                            ./insight_testsuite/tests/test_1/output/medianvals_by_zip.txt\
+                            ./insight_testsuite/tests/test_1/output/medianvals_by_date.txt
+python ./src/data_parse.py ./insight_testsuite/tests/test_2/input/itcont.txt\
+                            ./insight_testsuite/tests/test_2/output/medianvals_by_zip.txt\
+                            ./insight_testsuite/tests/test_2/output/medianvals_by_date.txt
+python ./src/data_parse.py ./insight_testsuite/tests/test_3/input/itcont.txt\
+                            ./insight_testsuite/tests/test_3/output/medianvals_by_zip.txt\
+                            ./insight_testsuite/tests/test_3/output/medianvals_by_date.txt
+'
